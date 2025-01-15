d49c2e37b7121120cd50f9720abf2a2befb74cc6
laraib610 <laraibkhan8242@gmail.com>
2025-01-15 17:47:45 +0500

    project page and package

--- main.js
+++ main.js
@@ -1,5 +1,109 @@
 $(function(){
     $("#header").load("shared/header/header.html"); 
-    $("#main-content").load("/home.html")
+    $("#partners").load("/shared/partners/partners.html", function() {
+      // This callback function ensures that the partners.html content is loaded before running your script
+     if(window.location.href.includes('/ind')){
+        console.log('working')
+        const wave = document.getElementById('wave-pink');
+        const wavehtml=` <img src="/assets/pink-wave.svg" class="purple-wave" style="z-index:1" alt="">
+        `
+        wave.innerHTML +=wavehtml;
+      } 
+       if(window.location.href.includes('package'))
+        {
+        const wave = document.getElementById('wave');
+        const wavehtml=`<img src="/assets/purple-wave.svg" class="purple-wave" alt="">
+        `
+        wave.innerHTML +=wavehtml;
+      }
+      
+      // Now you can safely run your JavaScript to modify #partners-container
+      const partnerImgUrls = [
+          { src: "/assets/samsung.png", width:"60%" },
+          { src: "/assets/amazon.png", width:"60%"},
+          { src: "/assets/apple.png" },
+          { src: "/assets/samsung.png", width:"60%" }
+      ];
+
+      // Select the container element where content will be inserted
+      const partnerContainer = document.getElementById('partners-container');
+
+      // Check if the partnerContainer exists before trying to modify it
+      if (partnerContainer) {
+          // Generate the HTML structure dynamically using a for loop
+          for (let i = 0; i < partnerImgUrls.length; i++) {
+              const cardHTML = `
+                  <div class="p-3">
+                    <div class=" d-flex justify-content-center" >
+                      <img src="${partnerImgUrls[i].src}" width=${partnerImgUrls[i].width} alt="">
+                    </div>
+                      
+                  </div>
+              `;
+              partnerContainer.innerHTML += cardHTML;
+          }
+      } else {
+          console.error("Element with id 'partners-container' not found!");
+      }
+
+      const globalOffices=[
+        {
+            heading:"PAKISTAN",
+            image:"/assets/pakistan.png",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            imagesize:"60%"
+        },
+        {
+            heading:"USA OFFICE",
+            image:"/assets/usa.png",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            imagesize:"79%"
+        },
+        {
+            heading:"UK OFFICE",
+            image:"/assets/uk.png",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            imagesize:"30%"
+        },
+        {
+            heading:"AUSTRALIA",
+            image:"/assets/australia.png",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            imagesize:"57%"
+        },
+        {
+          heading:"UAE OFFICE",
+          image:"/assets/uae.png",
+          description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            imagesize:"57%"
+      }
+      ];
+
+        const globalContainer = document.getElementById('global-office');
+
+    // Generate the HTML structure dynamically using a for loop
+    globalOffices.forEach(office => {
+        const cardHTML = `
+                     <div class=" p-3">
+                            <div class="">
+                                <div class="">
+                                    <img src="${office.image}" width=${office.imagesize} alt="">
+
+                                </div>
+                                <div class="py-2">
+                                    <h6 class="text-white">${office.heading}</h6>
+                                    <p class="text-white">${office.description}</p>
+                                </div>
+                                <span class="text-white">+92 123 145 4469</span>
+
+                            </div>
+
+                        </div>              
+
+        `;
+        globalContainer.innerHTML += cardHTML;
+    });
+
+  });
     $("#footer").load("shared/footer/footer.html"); 
   });
---
+++ scripts/projects.js
@@ -0,0 +1,184 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Array for content to be inserted into the elements
+
+    // Projects Cards
+    const projectTexts = [
+        "LOREM IPSUM IS SIMPLY DUMMY TEXT",
+        "LOREM IPSUM IS SIMPLY DUMMY TEXT",
+        "LOREM IPSUM IS SIMPLY DUMMY TEXT",
+        "LOREM IPSUM IS SIMPLY DUMMY TEXT",
+        "LOREM IPSUM IS SIMPLY DUMMY TEXT"
+    ];
+
+    // Select the container element where content will be inserted
+    const container = document.getElementById('project-container');
+
+    // Generate the HTML structure dynamically using a for loop
+    for (let i = 0; i < projectTexts.length; i++) {
+        const colClass = i === 0  ? 'col-lg-5' : i===1?'col-lg-7': 'col-lg-4';
+        const gridClass = (i === 0 || i === 4) ? 'grid_gradient' : '';
+        const cardHTML = `
+            <div class="${colClass} p-2">
+                <div class="card-projects ${gridClass} d-flex justify-content-center align-items-end">
+                    <p class="text-white mt-5">${projectTexts[i]}</p>
+                </div>
+            </div>
+        `;
+        container.innerHTML += cardHTML;
+    }
+
+
+    // community blobs cards
+
+    const blogPosts = [
+        {
+            title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+            category: "Community",
+            author: "ALAN WALKER",
+            date: "JANUARY 07, 2025",
+            imgSrc: "/assets/group-coworkers.png"
+        },
+        {
+            title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+            category: "Community",
+            author: "ALAN WALKER",
+            date: "JANUARY 07, 2025",
+            imgSrc: "/assets/group-coworkers.png"
+        },
+        {
+            title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+            category: "Community",
+            author: "ALAN WALKER",
+            date: "JANUARY 07, 2025",
+            imgSrc: "/assets/group-coworkers.png"
+        }
+    ];
+
+    // Select the container where content will be injected
+    const container2 = document.getElementById('blog-container');
+
+    // Generate the HTML structure dynamically using a for loop
+    blogPosts.forEach(post => {
+        const cardHTML = `
+            <div role="listitem" class="blob_item w-dyn-item col-lg-4">
+                <div class="blob_item p-2">
+                    <div class="py-5">
+                        <a aria-label="Blog Image Link" class="blog_image-link w-inline-block">
+                            <div class="blog_image-wrapper">
+                                <img src="${post.imgSrc}" loading="lazy" width="100%" alt="" class="blog_image">
+                            </div>
+                        </a>
+                    </div>
+                    <div>
+                        <h5 class="text-pink">${post.category}</h5>
+                        <h5 class="blob-heading">${post.title}</h5>
+                        <p class="blob-text">${post.description}</p>
+                        <div class="d-flex align-items-center">
+                            <img src="/assets/pink-circle.png" alt="">
+                            <sub class="text-secondary m-0 mx-2">By <a class="text-secondary text-decoration-underline">${post.author}&nbsp;&nbsp;&nbsp;&nbsp; ${post.date}</a></sub>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        container2.innerHTML += cardHTML;
+    });
+
+    // SERVICES CARDS
+
+    const services=[
+        {
+            heading:"WEB DEVELOPMENT",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            heading:"DESIGN",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            heading:"UI/UX DESIGN",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            heading:"APP DEVELOPMENT",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            heading:"WEB DEVELOPMENT",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            heading:"WEB DEVELOPMENT",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+    ]
+    // Select the container where content will be injected
+    const serviceContainer = document.getElementById('services');
+
+    // Generate the HTML structure dynamically using a for loop
+    services.forEach(service => {
+        const cardHTML = `
+            <div class="col-lg-4 p-3">
+                    <div class="card p-4">
+                        <h5 class="text-blue">${service.heading}</h5>
+                        <p>${service.description}</p>
+                    </div>
+
+                </div>
+        `;
+        serviceContainer.innerHTML += cardHTML;
+    });
+
+
+    // Teams Cards
+    const Teams=[
+        {
+            heading:"CEO",
+            image:"/assets/employee.png",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            heading:"CEO",
+            image:"/assets/employee.png",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            heading:"CEO",
+            image:"/assets/employee.png",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            heading:"CEO",
+            image:"/assets/employee.png",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        }];
+
+        const teamContainer = document.getElementById('team');
+
+    // Generate the HTML structure dynamically using a for loop
+    Teams.forEach(team => {
+        const cardHTML = `
+                     <div class="col-lg-3 p-3">
+                            <div class="employee">
+                                <div class="employee-image">
+                                    <img src="${team.image}" width="100%" alt="">
+
+                                </div>
+                                <div class="py-2">
+                                    <h6 class="text-white">${team.heading}</h6>
+                                    <p class="text-white">${team.description}</p>
+                                </div>
+
+                            </div>
+
+                        </div>              
+
+        `;
+        teamContainer.innerHTML += cardHTML;
+    });
+
+    
+});
