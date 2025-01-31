5defd7fae3702ce687699bde9d0d36db3ba8330c
laraib610 <laraibkhan8242@gmail.com>
2025-01-31 14:48:07 +0500

    changes

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,58 +1,73 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Array for content to be inserted into the elements
 
-    // Projects Cards
-    const projectTexts = [
-        "LOREM IPSUM IS SIMPLY DUMMY TEXT",
-        "LOREM IPSUM IS SIMPLY DUMMY TEXT",
-        "LOREM IPSUM IS SIMPLY DUMMY TEXT",
-        "LOREM IPSUM IS SIMPLY DUMMY TEXT",
-        "LOREM IPSUM IS SIMPLY DUMMY TEXT"
-    ];
+  // Projects Cards
+const projectTexts = [
+    "LOREM IPSUM IS SIMPLY DUMMY TEXT",
+    "LOREM IPSUM IS SIMPLY DUMMY TEXT",
+    "LOREM IPSUM IS SIMPLY DUMMY TEXT",
+    "LOREM IPSUM IS SIMPLY DUMMY TEXT",
+    "LOREM IPSUM IS SIMPLY DUMMY TEXT",
+    "LOREM IPSUM IS SIMPLY DUMMY TEXT",
+];
 
-    // Select the container element where content will be inserted
-    const container = document.getElementById('project-container');
-    // Generate the HTML structure dynamically using a for loop
-    for (let i = 0; i < projectTexts.length; i++) {
-        const colClass = i === 0  ? 'col-lg-5' : i===1?'col-lg-7': 'col-lg-4';
-        const gridClass = (i === 0 || i === 4) ? 'grid_gradient' : '';
-        const cardHTML = `
-            <div class="${colClass} p-2">
-                <div class="card-projects ${gridClass} d-flex justify-content-center align-items-end">
-                    <p class="text-white mt-5">${projectTexts[i]}</p>
-                </div>
+// Select the container element where content will be inserted
+const service_container = document.getElementById('services_card');
+
+// Group size for each carousel item (3 cards per slide)
+const cardsPerSlide = 3;
+
+for (let i = 0; i < projectTexts.length; i += cardsPerSlide) {
+    // For each group of 3 cards, create a single carousel item
+    const activeClass = i === 0 ? 'active' : ''; // Set the first item as active for carousel
+
+    // Create the carousel item and group 3 cards inside it
+    const cardHTML2 = `
+        <div class="carousel-item ${activeClass}">
+            <div class="d-flex justify-content-between">
+               
+                ${ createCard(i)}
+                ${ createCard(i + 1)}
+                ${ createCard(i + 2) }
             </div>
-        `;
-        container.innerHTML += cardHTML;
-    }
+        </div>
+    `;
+    service_container.innerHTML += cardHTML2;
+}
 
+// Function to create an individual card
+function createCard(index) {
+    return `
+        <div class="card-projects p-2 m-3">
+            <h3 class="text-white text-center my-2 py-4">Service</h3>
+            <p class="text-white my-5">${projectTexts[index]}</p>
+        </div>
+    `;
+}
 
 
     // community blobs cards
 
     const blogPosts = [
         {
-            title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-            category: "Community",
-            author: "ALAN WALKER",
-            date: "JANUARY 07, 2025",
+            title: "Atlassian Jira Software",
+            description: "Atlassian Jira is a popular project management tool designed for teams to plan, track, and release software.",
+            ceoQuote: "\"At Atlassian, our mission is to unleash the potential of every team. We believe that software should be intuitive and powerful.\" - Mike Cannon-Brookes",
+            author: "Mike Cannon-Brookes",
             imgSrc: "/assets/group-coworkers.png"
         },
         {
-            title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-            category: "Community",
-            author: "ALAN WALKER",
-            date: "JANUARY 07, 2025",
+            title: "Swyftx Cryptocurrency Exchange",
+            description: "Swyftx is an Australian cryptocurrency exchange platform offering low fees and secure transactions.",
+            ceoQuote: "\"We're committed to providing the best cryptocurrency exchange experience in Australia.\" - Ryan Edwards-Tierney",
+            author: " Ryan Edwards-Tierney",
             imgSrc: "/assets/group-coworkers.png"
         },
         {
-            title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-            category: "Community",
-            author: "ALAN WALKER",
-            date: "JANUARY 07, 2025",
+            title: "MYOB",
+            description: "MYOB provides accounting, payroll, and business management software for small to medium-sized businesses.",
+            ceoQuote: "\"We are focused on delivering software solutions that help Australian businesses thrive.\" - Greg Ellis",
+            author: "Greg Ellis",
             imgSrc: "/assets/group-coworkers.png"
         }
     ];
@@ -77,12 +92,13 @@ document.addEventListener('DOMContentLoaded', function () {
                         </a>
                     </div>
                     <div>
-                        <h5 class="text-pink">${post.category}</h5>
-                        <h5 class="blob-heading">${post.title}</h5>
-                        <p class="blob-text">${post.description}</p>
+                        <h5 class="text-pink">${post.title}</h5>
+                        <p class="blob-heading">${post.description}</p>
+                        <p class="blob-text">${post.ceoQuote}</p>
+
                         <div class="d-flex align-items-center">
                             <img src="/assets/pink-circle.png" alt="">
-                            <sub class="text-secondary m-0 mx-2">By <a class="text-secondary text-decoration-underline">${post.author}&nbsp;&nbsp;&nbsp;&nbsp; ${post.date}</a></sub>
+                            <sub class="text-secondary m-0 mx-2">By <a class="text-secondary text-decoration-underline">${post.author}</a></sub>
                         </div>
                     </div>
                 </div>
@@ -95,29 +111,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const services=[
         {
-            heading:"WEB DEVELOPMENT",
+            heading:"01.",
+            subheading:"You can trust",
             description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
         },
         {
-            heading:"DESIGN",
+            heading:"02.",
+            subheading:"You can trust",
             description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
         },
         {
-            heading:"UI/UX DESIGN",
+            heading:"03.",
+            subheading:"You can trust",
             description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
         },
         {
-            heading:"APP DEVELOPMENT",
+            heading:"04.",
+            subheading:"You can trust",
             description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
         },
-        {
-            heading:"WEB DEVELOPMENT",
-            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-        },
-        {
-            heading:"WEB DEVELOPMENT",
-            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-        },
+
+
     ]
     // Select the container where content will be injected
     const serviceContainer = document.getElementById('services');
@@ -125,10 +139,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Generate the HTML structure dynamically using a for loop
     services.forEach(service => {
         const cardHTML = `
-            <div class="col-lg-4 p-3">
-                    <div class="card p-4">
-                        <h5 class="text-blue">${service.heading}</h5>
-                        <p>${service.description}</p>
+            <div class="col-lg-3 p-3 ">
+                    <div class=" p-4">
+                        <h3 class="text-pink m-0">${service.heading}</h3>
+                        <h5 class="text-white">${service.subheading}</h5>
+                        <p class="text-white">${service.description}</p>
                     </div>
 
                 </div>
